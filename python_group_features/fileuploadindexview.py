50bca2e64fed8e2512b8e0cd14a03ec660dc511c
ncss133 <ncss133@fecbcdfb-b2d2-0410-aa5b-c9f85bfd5f58>
2013-01-12 04:10:35 +0000

    Sam- image upload code for merging with Max's login in prep for Design integration
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss133@44 fecbcdfb-b2d2-0410-aa5b-c9f85bfd5f58

--- python_group_features/fileuploadindexview.py
+++ python_group_features/fileuploadindexview.py
@@ -7,7 +7,7 @@ def home(response):
     <html>
         <body>
             <p>
-                   current pages availiable are <a href = "/upload" >/upload</a> and <a href = "/index"> /index <a>
+                   current pages avaliable are <a href = "/upload" >/upload</a> and <a href = "/index"> /index <a>
             </p>
         </body>
     <html>
@@ -22,7 +22,7 @@ def upload(response):
             <a href = "/home"> Home <a>
             <form id="upload" method="post" enctype="multipart/form-data">
                 <input type="file" name="upload_image"></br>
-                <input type="text" name="tags"> Tags (seperated by commas, cases are irrelevant) </input>
+                <input type="text" name="tags"> Tags (seperated by spaces, cases are irrelevant) </input>
                 <input type="submit" name="Submit" value="Upload Image"></br>
             </form>
         </body>
@@ -47,13 +47,13 @@ def upload(response):
         #static folder (static is safe)
         #we are writing raw bytes to file (that's how the site will recieve them)
         
-        with open("static/uploads/"+filename, 'wb') as f:
+        with open("static/uploads/images/"+filename, 'wb') as f:
 
             #write the raw data we got from the tuple to the file
             
             f.write(data)
 
-        with open("static/uploads/"+ filename.replace('.', '')+".txt", "w") as t:
+        with open("static/uploads/tags"+ filename.replace('.', '')+".txt", "w") as t:
 
             t.write(tags)
 
@@ -68,7 +68,10 @@ def view(response, name):
         <body>
             <a href = "/home"> home <a>
             <div>
-                <img src = "/static/uploads/"""+name+"""">
+                <img src = "/static/uploads/images/"""+name+"""">
+                <p>
+                """"""
+                </p>
                 </div>
         </body>
     </html>
@@ -83,7 +86,7 @@ def index(response):
         <a href = "/home"> Home <a>
             <div>
                 <p>""")
-    for file in os.listdir("static/uploads"):
+    for file in os.listdir("static/uploads/images"):
         response.write("<a href = /view/" + file + ">" + file +"</a></br>")
     response.write("""
                 </p>
