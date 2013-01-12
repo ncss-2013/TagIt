4e2d9d3cb3227f80afa7533fdae3ad0f3c5e481c
ncss133 <ncss133@fecbcdfb-b2d2-0410-aa5b-c9f85bfd5f58>
2013-01-12 04:12:36 +0000

    Updated so the login and logout forms/buttons are redirected to different pages where they are processed before being sent back to the index page. Displays different index page depending on whether the user is logged in or not. Still yet to interact with databases. Max
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss133@46 fecbcdfb-b2d2-0410-aa5b-c9f85bfd5f58

--- python_group_features/login_loggout.py
+++ python_group_features/login_loggout.py
@@ -6,6 +6,7 @@ cgitb.enable()
 
 def index(response): #Index
     if response.get_secure_cookie('login_cookie'):
+        #Cookie exists, display the logout button
         username_from_cookie = response.get_secure_cookie('login_cookie')
         response.write("Hello ")
         response.write(username_from_cookie)
@@ -13,7 +14,7 @@ def index(response): #Index
         <!doctype html>
         <html>
             <head></head>
-            <title>TagIt</title>
+            <title>Tag It</title>
             <body>
             <br />
             <button type = "button"
@@ -25,58 +26,44 @@ def index(response): #Index
             </body>
         </html>''')
 
-        logout = response.get_field('logout')
     else:
-        response.write("Generic home page")
-
-def login(response): #Login page
-    if response.get_secure_cookie('login_cookie'):
-        username_from_cookie = response.get_secure_cookie('login_cookie')
-        response.write("You are already logged in, ")
-        response.write(username_from_cookie)
-    else: #Here we enter the link to the HTML login page
+        #Cookie does not exist, display the login page
+        #Send the form to the '/login' page to process
         response.write('''
         <!doctype html>
         <html>
+            <title>Tag it</title>
             <head></head>
             <body>
-                <form id="login" method='POST'>
+                <form action="/login" id="login" method='POST'>
                     <input type="text" name="username"> Username </input>
                     <input type="password" name="password"> Password </input>
-                    <input type="submit" name="submit" value="Submit">
+                    <input type="submit" name="submit" value="Submit" </input>
                 </form>
-                <br />
                 Current username and password are 'chicken' and 'egg' . Working on database interaction
-                <br />
             </body>
         <html>''')
-        username = response.get_field('username')
-        password = response.get_field('password')
 
-        #Here we need to connect and check with the database
-        #Need to ensure against SQL injection attacks(?)
-        if username == 'chicken':
-            if password == 'egg':
-                response.set_secure_cookie('login_cookie', username)
-                response.write('Welcome back, ' + username + ".")
-        elif username and not password:
-            response.write('Please ensure you enter a password')
-        elif not username and password:
-            response.write('Please enter a username and a password')
+def login(response): #Login page
+    username = response.get_field('username')
+    password = response.get_field('password')
+    #Here we need to connect and check with the database
+    #Need to ensure against SQL injection attacks(?)
+    if username == 'chicken' and password == 'egg':
+        response.set_secure_cookie('login_cookie', username)
+        response.redirect('/')
+    else:
+        response.redirect('/')
+    
 
 def loggedout(response): #Loggedout page, delete cookies here
     response.clear_cookie('login_cookie')
     response.redirect('/')
 
-def upload(response): #Image upload page
-    response.write("<title>Upload an image</title>Yeah we're throwing science at walls here people")
-
 server = Server()
 server.register('/', index)
 server.register('/login', login)
 server.register('/loggedout', loggedout)
-server.register('/upload', upload)
 server.run()
 
-#To logout user, delete their cookie and then check
-#if the cookie is still there. Display page accordingly
+#Yeah we're just throwing science at walls here people"
