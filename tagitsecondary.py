8862722d38ca2b008f1ac3dd752b7ac868e06a99
ncss133 <ncss133@fecbcdfb-b2d2-0410-aa5b-c9f85bfd5f58>
2013-01-13 01:26:29 +0000

    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss133@111 fecbcdfb-b2d2-0410-aa5b-c9f85bfd5f58

--- tagitsecondary.py
+++ tagitsecondary.py
@@ -2,6 +2,8 @@
 from tornado import Server
 import sqlite3
 from template_language import *
+from db import *
+
 
 context = {}
 f = open('template/index.html', 'rU')
@@ -11,27 +13,32 @@ f.close()
 firstname,lastname, email, country, sex, age = "", "", "", "", "", 0
 
 def createlogin(response):
-    user = User.create(username, password)
-    user = User.find(username)
-    user = User('sam', 'password')
-    lastname = user.getlastname()
+##    user = User.create(username, password)
+##    user = User.find(username)
+##    user = User('sam', 'password')
+##    lastname = user.getlastname()
     #response.write(index_html)             #print the html for the page
-    conn = sqlite3.connect("database.db")   
-    cursor = conn.cursor()
+##    conn = sqlite3.connect("database.db")   
+##    cursor = conn.cursor()
     if response.get_secure_cookie('tag_it'):#check if we have the tag_it cookie (are we logged in?), so the user can't log in.
         response.write('You\'re already logged in, so you cannot create a new account - YOU HAVE ONE!') #in the future make it so it gives you the option to log out(delete cookies), and list who you're logged in as - allowing the user to create an account
     else:
-        response.write(index_html)          #print the submit form only if there is no log in cookie
+        response.write(create(index_html, context))          #print the submit form only if there is no log in cookie
         username = response.get_field('username')
         password = response.get_field('password')
         password_check = response.get_field('password_check')
+        email = response.get_field('email')
+        first_name = response.get_field('first_name')
+        last_name = response.get_field('last_name')
+        
         
         if(password_check == password and password_check is not None and username is not None):     #check if the two password fields are the same
             # SQL injection attack! vv
-            cursor.execute("INSERT INTO users VALUES ('" + str(username) + "', '" + str(password) + "', '" + str(firstname) + "', '" + str(lastname) + "', '" + str(email) + "', '" + str(country) + "', '" + str(sex) + "', " + str(age) +");")
-            conn.commit()
-            cursor.close()
-            conn.close()
+##            cursor.execute("INSERT INTO users VALUES ('" + str(username) + "', '" + str(password) + "', '" + str(firstname) + "', '" + str(lastname) + "', '" + str(email) + "', '" + str(country) + "', '" + str(sex) + "', " + str(age) +");")
+##            conn.commit()
+##            cursor.close()
+##            conn.close()
+            username_user = User.create(username, password, first_name, last_name, email, None, None, None) 
             response.set_secure_cookie('tag_it', username)  #make the user log in after sign up
             response.write("Well done, you created a user: " + username) #
         elif(password_check is None and password is None):
